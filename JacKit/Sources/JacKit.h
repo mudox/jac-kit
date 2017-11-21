44fc5c2f0bbb478903ed7d62e5434df6bf952b4d
mudox <imudox@gmail.com>
2017-11-21 11:36:52 +0800

    Remove space after `\0`

--- JacKit/Sources/JacKit.h
+++ JacKit/Sources/JacKit.h
@@ -3,17 +3,17 @@
 #define BOOLSTR(b) (( (b) ? @"YES" : @"NO" ))
 #define BOOLSYMBOL(b) (( (b) ? @"✓" : @"✗" ))
 
-#define JackError(content, ...)     DDLogError(@"%@ %@\0 " content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackInfo(content, ...)      DDLogInfo(@"%@ %@\0 " content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackWarn(content, ...)      DDLogWarn(@"%@ %@\0 " content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackDebug(content, ...)     DDLogDebug(@"%@ %@\0 " content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackVerbose(content, ...)   DDLogVerbose(@"%@ %@\0 " content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackError(content, ...)     DDLogError(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackInfo(content, ...)      DDLogInfo(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackWarn(content, ...)      DDLogWarn(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackDebug(content, ...)     DDLogDebug(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackVerbose(content, ...)   DDLogVerbose(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
 
-#define JackErrorWithPrefix(prefix, content, ...)     DDLogError(@"%@\0 " content, prefix, ## __VA_ARGS__)
-#define JackInfoWithPrefix(prefix, content, ...)      DDLogInfo(@"%@\0 " content, prefix, ## __VA_ARGS__)
-#define JackWarnWithPrefix(prefix, content, ...)      DDLogWarn(@"%@\0 " content, prefix, ## __VA_ARGS__)
-#define JackDebugWithPrefix(prefix, content, ...)     DDLogDebug(@"%@\0 " content, prefix, ## __VA_ARGS__)
-#define JackVerboseWithPrefix(prefix, content, ...)   DDLogVerbose(@"%@\0 " content, prefix, ## __VA_ARGS__)
+#define JackErrorWithPrefix(prefix, content, ...)     DDLogError(@"%@\0" content, prefix, ## __VA_ARGS__)
+#define JackInfoWithPrefix(prefix, content, ...)      DDLogInfo(@"%@\0" content, prefix, ## __VA_ARGS__)
+#define JackWarnWithPrefix(prefix, content, ...)      DDLogWarn(@"%@\0" content, prefix, ## __VA_ARGS__)
+#define JackDebugWithPrefix(prefix, content, ...)     DDLogDebug(@"%@\0" content, prefix, ## __VA_ARGS__)
+#define JackVerboseWithPrefix(prefix, content, ...)   DDLogVerbose(@"%@\0" content, prefix, ## __VA_ARGS__)
 
 @interface Jack : NSObject
 
