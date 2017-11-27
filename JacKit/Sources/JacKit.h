43fcc2f90caae3f1afc7dbd445b192a8fb39553a
mudox <imudox@gmail.com>
2017-11-27 21:56:24 +0800

    Improve macros

--- JacKit/Sources/JacKit.h
+++ JacKit/Sources/JacKit.h
@@ -3,17 +3,17 @@
 #define BOOLSTR(b) (( (b) ? @"YES" : @"NO" ))
 #define BOOLSYMBOL(b) (( (b) ? @"✓" : @"✗" ))
 
-#define JackError(content, ...)     DDLogError(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackInfo(content, ...)      DDLogInfo(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackWarn(content, ...)      DDLogWarn(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackDebug(content, ...)     DDLogDebug(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
-#define JackVerbose(content, ...)   DDLogVerbose(@"%@ %@\0" content, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackError(fmt, ...)     DDLogError(@"%@ %@\0" fmt, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackInfo(fmt, ...)      DDLogInfo(@"%@ %@\0" fmt, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackWarn(fmt, ...)      DDLogWarn(@"%@ %@\0" fmt, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackDebug(fmt, ...)     DDLogDebug(@"%@ %@\0" fmt, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
+#define JackVerbose(fmt, ...)   DDLogVerbose(@"%@ %@\0" fmt, THIS_FILE, THIS_METHOD, ## __VA_ARGS__)
 
-#define JackErrorWithPrefix(prefix, content, ...)     DDLogError(@"%@\0" content, prefix, ## __VA_ARGS__)
-#define JackInfoWithPrefix(prefix, content, ...)      DDLogInfo(@"%@\0" content, prefix, ## __VA_ARGS__)
-#define JackWarnWithPrefix(prefix, content, ...)      DDLogWarn(@"%@\0" content, prefix, ## __VA_ARGS__)
-#define JackDebugWithPrefix(prefix, content, ...)     DDLogDebug(@"%@\0" content, prefix, ## __VA_ARGS__)
-#define JackVerboseWithPrefix(prefix, content, ...)   DDLogVerbose(@"%@\0" content, prefix, ## __VA_ARGS__)
+#define JackErrorWithPrefix(prefix, fmt, ...)     DDLogError(@"%@\0" fmt, prefix, ## __VA_ARGS__)
+#define JackInfoWithPrefix(prefix, fmt, ...)      DDLogInfo(@"%@\0" fmt, prefix, ## __VA_ARGS__)
+#define JackWarnWithPrefix(prefix, fmt, ...)      DDLogWarn(@"%@\0" fmt, prefix, ## __VA_ARGS__)
+#define JackDebugWithPrefix(prefix, fmt, ...)     DDLogDebug(@"%@\0" fmt, prefix, ## __VA_ARGS__)
+#define JackVerboseWithPrefix(prefix, fmt, ...)   DDLogVerbose(@"%@\0" fmt, prefix, ## __VA_ARGS__)
 
 // used internally to log directly to terminal
 #define TTYLog(args,...) do { [(NSFileHandle*)[NSFileHandle fileHandleWithStandardOutput] writeData:[[NSString stringWithFormat:args, ##__VA_ARGS__] dataUsingEncoding: NSUTF8StringEncoding]]; } while(0);
