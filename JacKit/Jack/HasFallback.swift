72789c1eb2f3a91409c3e8a9d0065b4d0139f92a
mudox <imudox@gmail.com>
2018-09-23 13:32:06 +0800

    Add `HasFallback.swift`

---
+++ JacKit/Jack/HasFallback.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+import CocoaLumberjack
+
+internal protocol HasFallback: Equatable {
+  static var fallback: Self { get }
+}
+
+extension DDLogLevel: HasFallback {
+  static var fallback: DDLogLevel {
+    return .verbose
+  }
+}
