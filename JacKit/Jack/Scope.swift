1dce847e0114ca943e6da3a88b2163aedddb8142
mudox <imudox@gmail.com>
2018-09-23 13:37:19 +0800

    Add `Scope.swift`

---
+++ JacKit/Jack/Scope.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+import CocoaLumberjack
+
+internal extension Jack {
+
+  struct Scope {
+    static let fallback = Scope("FALLBACK")!
+
+    enum Kind {
+      case normal
+      case file
+    }
+
+    let string: String
+    let kind = Kind.normal
+
+    static func isValid(scopeString: String) -> Bool {
+      // If already already checked
+//      _lock.lock()
+//      if _scopeLevels.keys.contains(scopeString) {
+//        return true
+//      }
+//      _lock.unlock()
+
+      let components = scopeString.split(separator: ".", omittingEmptySubsequences: false)
+
+      guard !components.isEmpty else { return false }
+
+      //      for text in components {
+      //        if text.range(of: "^\\b.*\\b$", options: .regularExpression) == nil {
+      //          return false
+      //        }
+      //      }
+
+      return true
+    }
+
+    init?(_ string: String, kind: Kind = .normal) {
+      guard Scope.isValid(scopeString: string) else {
+        return nil
+      }
+
+      self.string = string
+    }
+
+    var superScopeStrings: [String] {
+
+      let first = string.components(separatedBy: ".")[...]
+
+      return sequence(first: first) {
+        let next = $0.dropLast()
+        return next.isEmpty ? nil : next
+      }.map {
+        $0.joined(separator: ".")
+      }
+    }
+
+  }
+
+}
+
