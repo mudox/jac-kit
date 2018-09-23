72b1009fe05324fd088ccd9c4179b7c6197eee91
mudox <imudox@gmail.com>
2018-09-23 14:31:13 +0800

    Add `Options.swift`

---
+++ JacKit/Jack/Options.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+import CocoaLumberjack
+
+extension Jack {
+  
+  public struct Options: OptionSet {
+    public let rawValue: Int
+
+    public init(rawValue: Int) {
+      self.rawValue = rawValue
+    }
+
+    // Primitive cases
+    public static let noLevelIcon = Options(rawValue: 1 << 0)
+    public static let noLocation = Options(rawValue: 1 << 1)
+    public static let noScope = Options(rawValue: 1 << 2)
+    
+    // Use as less lines as possible
+    public static let compact = Options(rawValue: 1 << 3)
+
+    // Derived cases
+    public static let `default`: Options = []
+    public static let bare: Options = [.noLevelIcon, .noLocation, .noScope]
+    public static let short: Options = [.noLocation, .compact]
+  }
+
+  public static var defaultOptions: Options = []
+  
+}
