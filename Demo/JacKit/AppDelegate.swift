9c9635278d4851029efc604119700357e2a94e9e
mudox <imudox@gmail.com>
2018-11-15 05:32:19 +0800

    Remove imports

--- Demo/JacKit/AppDelegate.swift
+++ Demo/JacKit/AppDelegate.swift
@@ -1,18 +1,18 @@
 import UIKit
 
-import MudoxKit
-
-import JacKit
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+  private func application(
+    _ application: UIApplication, didFinishLaunchingWithOptions
+    launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  )
+    -> Bool
+  {
 
     return true
   }
-
 
 }
