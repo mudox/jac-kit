dadb81d054be1758b3199ba1239d1388dbfab63a
mudox <imudox@gmail.com>
2018-11-18 03:10:53 +0800

    Fix: option -> foramt in json object

--- Demo/JacKit/AppDelegate.swift
+++ Demo/JacKit/AppDelegate.swift
@@ -8,8 +8,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   private func application(
-    _ application: UIApplication, didFinishLaunchingWithOptions
-    launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   )
     -> Bool
   {
--- Source/CLManager/JKTTYLoggerFormatter.m
+++ Source/CLManager/JKTTYLoggerFormatter.m
@@ -4,7 +4,7 @@
 
 // Must be sync with `Jack.Options`
 enum {
-    noLevelIcon = 1 << 0,
+    noIcon = 1 << 0,
     noLocation = 1 << 1,
     noScope = 1 << 2,
     compact = 1 << 3
@@ -42,7 +42,13 @@ NSString * iconForFlag(DDLogFlag flag) {
 
 - (NSString *)formatLogMessage:(DDLogMessage *)logMessage {
 
-    // Level icon
+    //
+    //
+    // Step 1 - Extract components
+    //
+    //
+
+    // Icon
     NSString * levelIcon = iconForFlag(logMessage.flag);
 
 
@@ -58,7 +64,8 @@ NSString * iconForFlag(DDLogFlag flag) {
     // Scope
     NSString * scope, * location, * message;
     scope = jsonObject[@"scope"];
-    scope = [NSString stringWithFormat:@"[%@]", scope];
+    scope = [scope stringByReplacingOccurrencesOfString:@"." withString:@"・"];
+    scope = [NSString stringWithFormat:@"%@", scope];
 
     // Location
     location = [NSString stringWithFormat:@"▹ %@・%@・%@", jsonObject[@"file"], jsonObject[@"function"], jsonObject[@"line"]];
@@ -72,22 +79,22 @@ NSString * iconForFlag(DDLogFlag flag) {
      *
      */
 
-    int options = [jsonObject[@"options"] intValue];
+    int format = [jsonObject[@"format"] intValue];
 
     NSString * formatted = nil;
 
-    switch (options) {
-        case noLevelIcon:
+    switch (format) {
+        case noIcon:
             formatted = [NSString stringWithFormat:@"%@\n%@\n%@", scope, message, location];
             break;
-        case noLevelIcon | compact:
-            formatted = [NSString stringWithFormat:@"%@   %@\n%@", scope, message, location];
+        case noIcon | compact:
+            formatted = [NSString stringWithFormat:@"%@ - %@\n%@", scope, message, location];
             break;
         case  noLocation:
             formatted = [NSString stringWithFormat:@"%@ %@\n%@", levelIcon, scope, message];
             break;
         case  noLocation | compact:
-            formatted = [NSString stringWithFormat:@"%@ %@   %@", levelIcon, scope, message];
+            formatted = [NSString stringWithFormat:@"%@ %@ - %@", levelIcon, scope, message];
             break;
         case  noScope:
             formatted = [NSString stringWithFormat:@"%@ %@\n%@", levelIcon, message, location];
@@ -95,20 +102,20 @@ NSString * iconForFlag(DDLogFlag flag) {
         case  noScope | compact:
             formatted = [NSString stringWithFormat:@"%@ %@\n%@", levelIcon, message, location];
             break;
-        case  noLevelIcon | noLocation:
+        case  noIcon | noLocation:
             formatted = [NSString stringWithFormat:@"%@\n%@", scope, message];
             break;
-        case  noLevelIcon | noLocation | compact:
-            formatted = [NSString stringWithFormat:@"%@   %@", scope, message];
+        case  noIcon | noLocation | compact:
+            formatted = [NSString stringWithFormat:@"%@ - %@", scope, message];
             break;
-        case  noLevelIcon | noLocation | noScope:
+        case  noIcon | noLocation | noScope:
             formatted = [NSString stringWithFormat:@"%@", message];
             break;
-        case  noLevelIcon | noLocation | noScope | compact:
+        case  noIcon | noLocation | noScope | compact:
             formatted = [NSString stringWithFormat:@"%@", message];
             break;
         case  compact:
-            formatted = [NSString stringWithFormat:@"%@ %@   %@\n%@", levelIcon, scope, message, location];
+            formatted = [NSString stringWithFormat:@"%@ %@ - %@\n%@", levelIcon, scope, message, location];
         default:
             formatted = [NSString stringWithFormat:@"%@ %@\n%@\n%@", levelIcon, scope, message, location];
     }
